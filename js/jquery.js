36c0bd84529b74e98a8c93dc9aacc85185c585c8
Jiaojiao Song <jiaojiaosong@roostify-jiaojiao-song2.lan>
2021-01-04 08:40:46 -0800

    most updated

--- js/jquery.js
+++ js/jquery.js
@@ -121,11 +121,11 @@ function checkSize(){
 }
 
 
-$(document).bind("contextmenu",function(e) {
-    e.preventDefault();
-   });
-   $(document).keydown(function(e){
-       if(e.which === 123){
-          return false;
-       }
-   });
+// $(document).bind("contextmenu",function(e) {
+//     e.preventDefault();
+//    });
+//    $(document).keydown(function(e){
+//        if(e.which === 123){
+//           return false;
+//        }
+//    });
